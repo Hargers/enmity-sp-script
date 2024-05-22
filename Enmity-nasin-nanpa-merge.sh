5090683527b92ce6cdfca3b6093e204958782474
Harger <30244834+Hargers@users.noreply.github.com>
2024-05-21 22:23:16 -0400

    edits plist to use OpenType fonts
    Edit FontForge script to remove conflicting ligature table and output OpenType fonts
    Edits plist to use OpenType fonts

--- Enmity-nasin-nanpa-merge.sh
+++ Enmity-nasin-nanpa-merge.sh
@@ -10,22 +10,25 @@ if [[ -v missing ]]; then
         echo "Aborting."; exit 1;
 fi
 
-wget -O nasin-nanpa.otf $(wget -q -O - https://api.github.com/repos/ETBCOR/nasin-nanpa/releases/latest | awk '/UCSUR/ && /url/ {print $2}' | sed 's/\"//g') -q --show-progress || { echo >&2 "Failed to download font nasin-nanpa"; exit 1; }
+wget -O nasin-nanpa.otf $(wget -q -O - https://api.github.com/repos/ETBCOR/nasin-nanpa/releases/latest | awk '/browser_download_url/ && $0 !~ /Helvetica/ && $0 !~ /UCSUR/ {print $2}' | sed 's/\"//g') -q --show-progress || { echo >&2 "Failed to download font nasin-nanpa"; exit 1; }
 wget https://github.com/enmity-mod/tweak/releases/latest/download/Enmity.ipa -q --show-progress || { echo >&2 "Failed to download Enmity"; exit 1; }
 
-fontforge -lang=ff -c "Open(\"nasin-nanpa.otf\"); Generate(\"nasin-nanpa.ttf\")"
+fontforge -lang=ff -c "Open(\"nasin-nanpa.otf\"); RemoveLookup(\"'liga' Standard Ligatures in Latin lookup 1\"); Generate(\"nasin-nanpa.otf\")"
 
 unzip Enmity.ipa
 
 for font in Payload/Discord.app/*.ttf; do
-	echo "$font"
+	echo "$font -> ${font%???}otf"
 	fontforge -lang=ff -c "Open(\"$font\");
-	MergeFonts(\"nasin-nanpa.ttf\");
-	Generate(\"$font\")"
+	MergeFonts(\"nasin-nanpa.otf\");
+	Generate(\"${font%???}otf\")"
+	rm $font
 done
+
+sed -i -e 's/ttf/otf/g' Payload/Discord.app/Info.plist
 
 zip -r Enmity-SP.ipa Payload/
 echo "File output to Enmity-SP.ipa"
 echo "Cleaning up..."
-rm -r Payload Enmity.ipa nasin-nanpa.otf nasin-nanpa.ttf
+rm -r Payload Enmity.ipa nasin-nanpa.otf
 echo "Done."
